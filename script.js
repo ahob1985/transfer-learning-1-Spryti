2a14abe4ba0304b4305890ae2961d545af2673d8
Spryti <67284103+Spryti@users.noreply.github.com>
2021-06-29 18:51:58 +0000

    Almost Finished

--- script.js
+++ script.js
@@ -33,42 +33,74 @@ function setup() {
   isModelReady = false;
   isTrainingComplete = false;
   video = createCapture(VIDEO, videoReady);
+  video.parent(canvasDiv);
 }
 
 function draw() {
-
+  if(isModelReady) {
+    image(video, 0, 0);
+  }
+  if(isTrainingComplete) {
+    classifier.classify(canvas, gotResults);
+  }
 }
 
 function buildButtons() {
   buttonDiv = createDiv();
   happyButton = createButton("Happy");
   happyButton.parent(buttonDiv);
-   happyButton.mousePressed(function() {
-     happies += 1;
-     textP.html("Happies: " + happies + "Sads")
-   })
+  happyButton.mousePressed(function() {
+    happies++;
+    textP.html("Happies: " + happies + "Sads: " + sads);
+    classifier.addImage(canvas, "Happy");
+  });
   sadButton = createButton("Sad");
   sadButton.parent(buttonDiv);
-
-
+  sadButton.mousePressed(function() {
+    sads += 1;
+    textP.html("Happies: " + happies + "Sads" + sads);
+    classifier.addImage(canvas, "Sad");
+  });
+  trainButton = createButton("Train");
+  trainButton.parent(buttonDiv);
+  trainButton.mousePressed(function() {
+    buttonDiv.style("display", "none");
+    textP.html("New model training, pleas wait...");
+    classifier.train(whileTraining);
+  });
+  buttonDiv.style("display", "none");
 }
 
 function videoReady() {
-
+  video.style("display", "none");
+  featureExtractor = ml5.featureExtractor("MobileNet", featureExtractorLoaded);
 }
 
 function featureExtractorLoaded() {
+  classifier = featureExtractor.classification(canvas, modelReady);
 
 }
 
 function modelReady() {
-
+  isModelReady = true;
+  textP.html("Add training data, then click train");
+  buttonDiv.style("display", "block");
 }
 
 function whileTraining(loss) {
-
+  if(loss) {
+    console.log(loss);
+  } else {
+    isTrainingComplete = true;
+  }
 }
 
 function gotResults(error, results) {
-
+   if(error) {
+     console.error(error);
+   } else {
+     let label = results[0].label;
+     let confidence = round(results[0].confidence, 2);
+     textP.html("Label: " + label + "Confidence:" + confidence);
+   }
 }
